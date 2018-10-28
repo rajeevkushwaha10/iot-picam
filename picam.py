dcb4a5f443d772696cf9e229cb5ffe633021d8da
rajeevkushwaha10 <rajeevkushwaha10@gmail.com>
2018-10-28 16:30:46 +0530

    Add files via upload

---
+++ picam.py
@@ -0,0 +1,11 @@
+from time import sleep
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/Pictures/newImage.jpg')
+camera.stop_preview()
+
+
